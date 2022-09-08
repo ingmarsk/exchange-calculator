cc38746bf8f7384a50d6198af0c63795e6964f2b
Ingemar Andrade <ingmar.lbs@gmail.com>
2022-09-09 01:11:08 +0200

    Refactor

--- app.rb
+++ app.rb
@@ -5,12 +5,10 @@ require 'currencies'
 require 'httparty'
 require 'pry-byebug'
 
-# load local files
-require_relative 'lib/cli'
-require_relative 'lib/services/application_service'
-require_relative 'lib/services/exchange_rate_api_services/currency_rates_service'
-require_relative 'lib/services/currency_services/convert_currency_service'
-require_relative 'lib/services/cli_services/user_input_service'
+# load local ruby files
+Dir.glob(File.join('lib', '**', '*.rb')).each do |file|
+  require_relative file
+end
 
 # Start program
 CLI.start
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,9 @@ require 'currencies'
 require 'httparty'
 require 'pry-byebug'
 
-# local clases
-require 'services/application_service'
-require 'services/exchange_rate_api_services/currency_rates_service'
-require 'services/currency_services/convert_currency_service'
-require 'services/cli_services/user_input_service'
-require 'cli'
-
 WebMock.disable_net_connect!(allow_localhost: true)
+
+# load local ruby files
+Dir.glob(File.join('lib', '**', '*.rb')).each do |path|
+  require path.gsub('lib/', '')
+end
